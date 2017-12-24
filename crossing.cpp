caaae68d2709f2eaa7ea9aa06eb0aa9d53076a2e
rin_chan <ritesh.goru@gmail.com>
2017-12-24 12:14:23 +0530

    Update crossing.cpp

--- crossing.cpp
+++ crossing.cpp
@@ -5,11 +5,11 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
+#include <math.h>
 using namespace cv;
 using namespace std;
 //global
-Mat img,adp,blurr,canny,hou;
+Mat img,adp,blurr,canny,hou,final;
 int max_value = 255;
 int odd_ker = 60;//or 70-97 for gaussian//20-40 for mean
 int ker_size = 2*odd_ker + 3;
@@ -120,20 +120,80 @@ void hough(int ,void*)
 	clusterise(slope,pointer,max_diff);
 	//filter the cluster
 	cluster_filter(cluster,cluster_point,min_size);
+	final = img.clone();
 	if (cluster.size()==1)
 	{
 		//zebra crossing is that one only
+		for(int i=0;i<cluster_point[0].size();i++)
+		{
+			Vec4i l = linesP[cluster_point[0][i]];
+        	line( final, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,0,0), 3, LINE_AA);
+		}
 	}
 	else if(cluster.size()==0)
 	{
-		cout<<"code screwed up";
+		cout<<"code screwed up"<<endl;
 	}
 	else if(cluster.size()>1)
 	{
-		//RANSAC
-	}
-	waitKey(0);
+		//linefit on each cluster
+		//define each mid point for each clusters initialise a 2d vector
+		vector<Vec2i> mid;
+		std::vector<float> value;
+		std::vector<int> value_point;
+		Vec4i q;
+		for(int i=0;i<cluster.size();i++)
+		{	//i is the number or the index of the cluster
+			for(int j=0;j<cluster[i].size();j++)
+			{
+				q = linesP[cluster_point[i][j]];
+				Vec2i extra_temp = ((q[0]+q[2])/2,(q[1]+q[3])/2);
+				mid.push_back(extra_temp);
+			}
+			//bestfit by method of least squares SD from that
+			//define the parameters
+			float m=0;
+			float b=0;
+			float x1=0;
+			float y1=0;
+			float temp1=0;
+			float temp2=0;
+			for(int k =0;k<mid.size();k++)
+			{
+				x1 = x1+mid[k][0];
+				y1 = y1+mid[k][1];
+			}
+			x1=x1/mid.size();
+			y1=y1/mid.size();
+			for(int k =0;k<mid.size();k++)
+			{
+				temp1 = temp1+((mid[k][0]-x1)*(mid[k][1]-y1));
+				temp2 = temp2+((mid[k][0]-x1)*(mid[k][0]-x1));
+			}
+			m = temp1/temp2;
+			b = y1-m*x1;
+			//got all the values for best fit
+			//summation of distances
+			for(int k =0;k<mid.size();k++)
+			{
+				temp1 = temp1+abs((m*mid[k][0]-mid[k][1]+b)/sqrt((1+m*m)));
+			}
+			value.push_back(temp1/mid.size());
+			value_point.push_back(i);
 
+			mid.clear();
+		}
+		selectionsort(value,value_point);
+		//value_point[0] is min cluster number
+		for(int i=0;i<cluster_point[value_point[0]].size();i++)
+		{
+			Vec4i l = linesP[cluster_point[value_point[0]][i]];
+        	line( final, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,0,0), 3, LINE_AA);
+		}
+	
+	}
+	imshow("final",final);
+	waitKey(0);
 }
 
 int main()
